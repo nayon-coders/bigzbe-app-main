da4d774b6c2d950d0168059590be678d0f58b9ca
NAYON <nayon@NAYONs-MacBook-Pro.local>
2022-10-11 18:07:05 +0600

    update and relete at 11-10-2022

--- android/app/src/main/kotlin/app/bigzbe/com/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package app.bigzbe.com
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/bigzbe_user/app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.bigzbe_user.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
